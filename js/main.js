fcbe16de924d4c2cf01289151d0e9627626c04ec
Connor Webdale <CWebdale006@west-mec.org>
2018-12-04 15:27:01 -0700

    Javascript and List images
    Fixed padding and margin in several elements. Started adding JavaScript to the skills.html

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function hideName() {
+    document.getElementsByName("h2").style.visibility = 'hidden';
+    document.getElementsByName("P").style.visibility = 'visible';
+    // document.getElementById('p1').style.visibility = 'visible';
+    // <p id="p1">
+}
+
+function hideDesc() {
+    document.getElementsByName("P").style.visibility = 'hidden';
+    document.getElementsByName("h2").style.visibility = 'visible';
+
+
+}
+
+
+
+
+
+
+// function hideName() {
+//     document.getElementById('h1').style.visibility = 'hidden';
+//     document.getElementsByTagName("P").style.visibility = 'visible';
+//     // document.getElementById('p1').style.visibility = 'visible';
+//     // <p id="p1">
+// }
+
+// function hideDesc() {
+//     document.getElementById('p1').style.visibility = 'hidden';
+//     document.getElementById('h1').style.visibility = 'visible';
+
+
+// }
