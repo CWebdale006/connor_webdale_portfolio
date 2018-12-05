384dfb325d468ab7df47e279c2429f9a4d4cd25c
Connor Webdale <CWebdale006@west-mec.org>
2018-12-05 15:24:26 -0700

    Contact/Skills html
    Fixed the list-style-images on the contact sheet by linking to an online stylesheet. Added JavaScript functions for each section on the skills.html. Need to fix the size of the background image when the screen size shrinks.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,70 @@
-function hideName() {
-    document.getElementsByName("h2").style.visibility = 'hidden';
-    document.getElementsByName("P").style.visibility = 'visible';
-    // document.getElementById('p1').style.visibility = 'visible';
-    // <p id="p1">
-}
-
-function hideDesc() {
-    document.getElementsByName("P").style.visibility = 'hidden';
-    document.getElementsByName("h2").style.visibility = 'visible';
-
+// Hides an h2 element and shows a paragraph element when a section is hovered
+function hideName1() {
+    document.getElementById('h1').style.visibility = 'hidden';
+    document.getElementById('p1').style.visibility = 'visible';
 
 }
 
+// Shows an h2 element and hides a paragraph element when the section is no longer hovered. 
+function hideDesc1() {
+    document.getElementById('p1').style.visibility = 'hidden';
+    document.getElementById('h1').style.visibility = 'visible';
 
 
+}
 
 
+function hideName2() {
+    document.getElementById('h2').style.visibility = 'hidden';
+    document.getElementById('p2').style.visibility = 'visible';
 
-// function hideName() {
-//     document.getElementById('h1').style.visibility = 'hidden';
-//     document.getElementsByTagName("P").style.visibility = 'visible';
-//     // document.getElementById('p1').style.visibility = 'visible';
-//     // <p id="p1">
-// }
+}
 
-// function hideDesc() {
-//     document.getElementById('p1').style.visibility = 'hidden';
-//     document.getElementById('h1').style.visibility = 'visible';
+function hideDesc2() {
+    document.getElementById('p2').style.visibility = 'hidden';
+    document.getElementById('h2').style.visibility = 'visible';
 
 
-// }
+}
+
+
+function hideName3() {
+    document.getElementById('h3').style.visibility = 'hidden';
+    document.getElementById('p3').style.visibility = 'visible';
+
+}
+
+function hideDesc3() {
+    document.getElementById('p3').style.visibility = 'hidden';
+    document.getElementById('h3').style.visibility = 'visible';
+
+
+}
+
+
+function hideName4() {
+    document.getElementById('h4').style.visibility = 'hidden';
+    document.getElementById('p4').style.visibility = 'visible';
+
+}
+
+function hideDesc4() {
+    document.getElementById('p4').style.visibility = 'hidden';
+    document.getElementById('h4').style.visibility = 'visible';
+
+
+}
+
+
+function hideName5() {
+    document.getElementById('h5').style.visibility = 'hidden';
+    document.getElementById('p5').style.visibility = 'visible';
+
+}
+
+function hideDesc5() {
+    document.getElementById('p5').style.visibility = 'hidden';
+    document.getElementById('h5').style.visibility = 'visible';
+
+
+}
